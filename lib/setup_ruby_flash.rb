3007e8478ffba03c992274a0683ad784e8460e40
Peter H. Boling <peter.boling@gmail.com>
2026-01-20 17:06:52 -0700

    📌 Pin version
    - Rename module (unused)

--- lib/setup_ruby_flash.rb
+++ lib/setup_ruby_flash.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-# SetupRv Acceptance Tests
+# SetupRubyFlash Acceptance Tests
 #
 # These tests run AFTER the GitHub Action has executed.
 # They verify that the action correctly installed Ruby, rv, and ore.
 #
 # The actual action logic is in action.yml (pure bash).
 # Ruby is only available for testing because the action installed it.
-module SetupRv
-  VERSION = '0.1.0'
+module SetupRubyFlash
+  VERSION = '1.4.0'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'setup_rv'
+require 'setup_ruby_flash'
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
